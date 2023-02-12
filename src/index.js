df4a2217b4d98bc664dc820be56b4fed9db5b85f
Mark Wang <carbonylgp@gmail.com>
2023-02-12 10:37:14 -0800

    overtake

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './res/styles.css';
-import Carbonyl from './Carbonyl';
-
-ReactDOM.render(<Carbonyl />, document.getElementById('root'));
